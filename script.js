daa21d4ccf5509b5291932001c385bc3e905c32b
Wall Liu <tuongst689@gmail.com>
2025-04-26 16:58:44 +0700

    make it better with css and darkmode

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function goToLink(url) {
       window.open(url, '_blank');
     }
+    
+    // Dark Mode Toggle
+    const toggle = document.getElementById('modeToggle');
+    toggle.addEventListener('change', function() {
+      document.body.classList.toggle('dark', this.checked);
+    });
+    
