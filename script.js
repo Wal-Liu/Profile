95a64929ee2922354a9d97557c70600770a6a988
Wall Liu <tuongst689@gmail.com>
2025-04-27 16:05:44 +0700

    avatar and dark mode

--- script.js
+++ script.js
@@ -2,9 +2,4 @@ function goToLink(url) {
       window.open(url, '_blank');
     }
     
-    // Dark Mode Toggle
-    const toggle = document.getElementById('modeToggle');
-    toggle.addEventListener('change', function() {
-      document.body.classList.toggle('dark', this.checked);
-    });
     
